56654da24ef909990745b68d28eb79d8f1b86649
priyanka pawar <ppp762002@gmail.com>
2024-09-25 09:09:13 -0700

    Control Statements

---
+++ Control statements/Putchar putch use.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch='Q',Grade='C',Group='X';
+
+    putch('n');
+    putchar('\t');
+
+    printf("Welcome to putch / putchar demonstration\n\n");
+    putch(ch);
+    putch('\n');
+
+    putch(ch);
+    putch('\n');
+
+    putch(Group);
+    putch('\n');
+
+    putch('s');
+    putch('\n');
+
+    putch('karad');
+    putch('\n');
+
+    putch('grade');
+    putch('\n');
+
+    getch();
+    return 0;
+}
